78d0f8bc57bcde8f62dbf7e17602058455c77afd
francisco.barbosa <francisco.barbosa@mv.com.br>
2023-05-07 14:19:13 -0300

    header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { Header } from './components/Header'
 import './global.css'
 
 function App() {
 
   return (
-    <>
-      <h1> oi </h1>
-    </>
+    <div>
+      <Header />
+    </div>
   )
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import rocketLogo from "../assets/rocket-logo.svg";
+import styles from "./Header.module.css";
+
+export function Header() {
+  return (
+    <div className={styles.header}>
+      <img src={rocketLogo} alt="Logo da rocketseat" />
+
+      <text>todo</text>
+    </div>
+  );
+}
