4d1e340a36a1c916c9db47f097520eb5766bdc61
francisco.barbosa <francisco.barbosa@mv.com.br>
2023-05-21 04:52:36 -0300

    fix img import on header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
-import rocketLogo from "../assets/Rocket-logo.svg";
+import RockedLogo from "../assets/RocketLogo.svg";
 import styles from "./Header.module.css";
 
 export function Header() {
   return (
     <div className={styles.header}>
-      <img src={rocketLogo} alt="Logo da rocketseat" />
+      <img src={RockedLogo} alt="Logo da rocketseat" />
 
       <strong>todo</strong>
     </div>
